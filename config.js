fadebc1e73bfc99f1942521df0fe9e170bbffe53
miniarmy946-ship-it <miniarmy946@gmail.com>
2026-02-02 01:08:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -28,9 +28,9 @@ const CONFIG = {
             secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
         },
         second: {
-            text: "How much do you love me?",                          // For the love meter
+            text: "kitna lekin??",                          // For the love meter
             startText: "This much!",                                   // Text before the percentage
-            nextBtn: "Next ❤️"                                         // Text for the next button
+            nextBtn: "Lae fir ❤️"                                         // Text for the next button
         },
         third: {
             text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
@@ -42,15 +42,15 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "Rae fir sherni hi kehde 🥰🚀💝",  // Shows when they go past 5000%
+        high: "Thoda sa aur karde! 🚀💝",              // Shows when they go past 1000%
+        normal: "bas chutu 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        title: "Ab wow amazing ki baari 🎉💝💖💝💓",
+        message: "Now come get your gift, a big warm hug and a huge kiss aur kal daaru ",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
